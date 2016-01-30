b0692c08ab636a8a7b54f14cca4ac79b300cf5fd
mehrik <sobreo.m@gmail.com>
2016-01-29 16:28:33 -0800

    Able to play music from other apps while recording a video. Need to first start up music app and then open up MVR app to work

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -18,7 +18,7 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
     
     @IBOutlet weak var bottomView: UIView!
     
-    var recording = true
+    var recording = false
     
     var captureSession: AVCaptureSession?
     var previewLayer: AVCaptureVideoPreviewLayer?
@@ -29,6 +29,10 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let session = AVAudioSession.sharedInstance()
+        // Allows sound from other apps to not be stopped
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: AVAudioSessionCategoryOptions.MixWithOthers)
         
         PHPhotoLibrary.requestAuthorization({(status:PHAuthorizationStatus) in
             switch status{
@@ -56,24 +60,43 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
         captureSession = AVCaptureSession()
         captureSession?.sessionPreset = AVCaptureSessionPresetHigh
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        cameraDevice = getFrontCamera()
+        cameraDevice = getCameraWithPosition(AVCaptureDevicePosition.Front)
         cameraDeviceInput = try! AVCaptureDeviceInput(device: cameraDevice!)
         
+        NSLog("Other devices playing: %@", session.otherAudioPlaying)
+        NSLog("Available inputs: %@", session.availableInputs!)
+        NSLog("Input Data Sources: %@", session.inputDataSources!)
+        NSLog("Inputs: %@", session.currentRoute.inputs)
+        NSLog("Outputs: %@", session.currentRoute.outputs)
+        
+        let audioDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeAudio)
+        let audioDeviceInput = try! AVCaptureDeviceInput(device: audioDevice!)
+        
         // Capture session has cameraDevice as an input
-        // AVCapture Input
+        // Must input audio first so able to swap out the camera stuff for later
+        // AVCapture input Microphone
+        if ((captureSession?.canAddInput(audioDeviceInput)) != nil) {
+            captureSession?.addInput(audioDeviceInput)
+        }
+        
+        // AVCapture input Camera
         if ((captureSession?.canAddInput(cameraDeviceInput)) != nil) {
             captureSession?.addInput(cameraDeviceInput)
         }
         
         // MARK: Output
         movieOutput = AVCaptureMovieFileOutput()
+        movieOutput?.movieFragmentInterval = kCMTimeInvalid
         if ((captureSession?.canAddOutput(movieOutput)) != nil) {
             captureSession?.addOutput(movieOutput)
         }
         
         // MARK: Preview Layer ---------------------------------------------
         previewLayer!.frame = view.bounds
-        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+        previewLayer?.videoGravity = AVLayerVideoGravityResize
+        
+        // Orientation of previewLayer DOES NOT effect the encoding of the actual video file
+        // Need to figure out how to save the video file rotated
         previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
         view.layer.addSublayer(previewLayer!)
         
@@ -86,6 +109,32 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if (!recording) {
+            let session = AVAudioSession.sharedInstance()
+            // Allows sound from other apps to not be stopped
+            try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: AVAudioSessionCategoryOptions.MixWithOthers)
+            if ((captureSession) != nil) {
+            captureSession?.beginConfiguration()
+            let currentCameraInput = captureSession?.inputs[1] as! AVCaptureInput
+            
+            captureSession?.removeInput(currentCameraInput)
+            
+            if cameraDevice!.position == AVCaptureDevicePosition.Front {
+                cameraDevice = getCameraWithPosition(AVCaptureDevicePosition.Back)
+            } else {
+                cameraDevice = getCameraWithPosition(AVCaptureDevicePosition.Front)
+            }
+            let newDeviceInput = try! AVCaptureDeviceInput(device: cameraDevice)
+            captureSession?.addInput(newDeviceInput)
+            print(captureSession?.inputs)
+            
+            captureSession?.commitConfiguration()
+            }
+        }
+        
     }
     
     //////////////////////////////////////////////////////////////////////////////////////////
@@ -126,12 +175,13 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
     
     func saveMovie() {
         movieOutput?.stopRecording()
+        
     }
     
-    func getFrontCamera() -> AVCaptureDevice? {
+    func getCameraWithPosition(position: AVCaptureDevicePosition) -> AVCaptureDevice? {
         let devices = AVCaptureDevice.devices()
         for device in devices {
-            if device.position == AVCaptureDevicePosition.Front {
+            if device.position == position {
                 return device as? AVCaptureDevice
             }
         }
@@ -155,7 +205,16 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
             try! fileManager.removeItemAtPath(filePathToDelete)
             print(filePathToDelete)
         }
-        
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        previewLayer!.frame = view.bounds
+        previewLayer?.videoGravity = AVLayerVideoGravityResize
+        print(previewLayer!.frame)
+        print(view.bounds)
+        print(size)
+        print(coordinator)
+        print(UIDevice.currentDevice().orientation)
     }
     
     //////////////////////////////////////////////////////////////////////////////////////////
@@ -164,16 +223,20 @@ class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelega
     //
     //////////////////////////////////////////////////////////////////////////////////////////
     @IBAction func recordButtonPressed(sender: UIButton) {
-        if (recording) {
+        if (!recording) {
             sender.backgroundColor = UIColor.magentaColor()
             print("Now recording")
             self.recordMovie()
-            recording = false
+            recording = true
         } else {
             sender.backgroundColor = UIColor.cyanColor()
             print("Saving movie...")
             self.saveMovie()
-            recording = true
+            recording = false
         }
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
     }
 }
